ae9986e0ecbf906977e6864999480bf3664d4643
elchuche <elchuche@naver.com>
2019-06-25 23:52:36 +0900

    기능 완성

--- app/src/main/java/com/example/nabot/App.java
+++ app/src/main/java/com/example/nabot/App.java
@@ -7,7 +7,7 @@ import android.support.v4.content.ContextCompat;
 
 public class App extends Application
 {
-    // instance variables
+    // 기능완성
     private static Context context;
 
     //******************************************************************************
